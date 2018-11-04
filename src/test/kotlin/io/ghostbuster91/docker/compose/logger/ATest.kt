f54d9b096b2635190bbbfbcf8a6aea527765f6e1
ghostbuster91 <kghost0@gmail.com>
2018-11-04 09:57:05 +0100

    Stream concatenation

--- src/test/kotlin/io/ghostbuster91/docker/compose/logger/ATest.kt
+++ src/test/kotlin/io/ghostbuster91/docker/compose/logger/ATest.kt
@@ -1,11 +1,22 @@
 package io.ghostbuster91.docker.compose.logger
 
+import io.reactivex.Observable
 import org.junit.Test
 
 class ATest {
 
     @Test
     fun name() {
-        println("crypto-node-proxy_1".startsWith("crypto-node-proxy"))
+        Observable.fromIterable(listOf(1,1,2,2,1,1))
+                .scan(""){ acc, item ->
+                    if(item == 1){
+                        "1"
+                    }else{
+                        acc + item
+                    }
+                }
+                .skip(1)
+                .doAfterNext { println(it) }
+                .blockingSubscribe()
     }
 }
