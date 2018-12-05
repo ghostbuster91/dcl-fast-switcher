99329676fa9e69a6b7d35e7e9d900ac07b5c09d9
ghostbuster91 <kghost0@gmail.com>
2018-12-05 09:33:17 +0100

    Fix shiftHandler implementation

--- src/main/kotlin/io/ghostbuster91/docker/compose/logger/keyboard/DefaultShiftRemoveServiceMapping.kt
+++ src/main/kotlin/io/ghostbuster91/docker/compose/logger/keyboard/DefaultShiftRemoveServiceMapping.kt
@@ -6,12 +6,21 @@ import io.ghostbuster91.docker.compose.logger.UserCommand
 class DefaultShiftRemoveServiceMapping(private val serviceMapping: ServiceMapping) : RemoveServiceMapping {
 
     override fun isRemoveKey(keyStroke: KeyStroke): Boolean {
-        return serviceMapping.isServiceKey(keyStroke.character?.toString()?.toLowerCase())
-            && !keyStroke.isAltDown
+        return keyStroke.character?.toString()
+                ?.let {
+                    serviceMapping.isServiceKey(it.toLowerCase())
+                            && !keyStroke.isAltDown
+                            && it.isUpperCase()
+                }
+                ?: false
     }
 
     override fun mapRemoveKey(keyStroke: KeyStroke): UserCommand.Remove {
         val service = serviceMapping.mapToService(keyStroke.character!!.toString().toLowerCase())
         return UserCommand.Remove(service)
+    }
+
+    fun String.isUpperCase(): Boolean {
+        return this.toUpperCase() == this
     }
 }
